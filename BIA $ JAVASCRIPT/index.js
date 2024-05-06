3af2222db90ff25eb99890c2e0ae9fea2338a87b
Shivam Singh gautam <105829038+Shivu150199@users.noreply.github.com>
2024-05-06 16:01:03 +0530

    06-05-2024

---
+++ BIA $ JAVASCRIPT/index.js
@@ -0,0 +1,286 @@
+'use strict';
+// name='lokendra'
+
+
+// section='bia4'
+
+// role='web developer'
+
+
+//console.log(name)
+/*console.log(section)
+console.log(role)
+
+console.log('bhagwan das')*/
+// Comment
+
+
+// single line 
+// multiline comment
+
+
+
+// hello my name is shivam
+
+// Lorem ipsum dolor sit amet c
+// onsectetur adipisicing elit. Corporis, modi.
+
+
+// var name
+
+// name='akash'
+
+
+// console.log(name)
+
+// var age
+
+// age=20
+
+// console.log(age)
+
+
+// var isEligibleToVote
+
+// isEligibleToVote=true
+
+// console.log(isEligibleToVote)
+
+// // var name2
+// // name2='aneesh'
+
+// var name2='aneesh'
+
+// variabletype variablename=variabelValue
+
+// favLanguage='javascript'
+
+// console.log(favLanguage)
+
+// var
+// const
+// let
+
+// job=student
+// job=emplo
+
+// var name='Etesh'
+// console.log(name)
+
+// var name='bahgwaan'
+
+// console.log(name)
+
+// var num=20
+// var num=30
+
+// let name='aneesh'//#300
+// console.log(name)
+// name='bhagwaan'//#400
+// console.log(name)
+
+
+// var = reassign , redeclare
+// let = redeclare =false reassign=true
+//const =redeclare=false reassign=false
+
+// const percent='61%'
+// console.log(percent)
+// percent='64%'
+// console.log(percent)
+
+
+// let age
+// age=23
+// let age=23
+
+
+// const age=20
+// var age1=24
+// let age1=45
+// age=24
+// console.log(age)
+
+
+
+// let name='bhagwan'
+// console.log('hello my name is ',name)
+// console.log(23)
+// let section='bia4'
+
+
+
+""
+
+
+// let number;
+
+// number=23
+// console.log(number)
+
+
+//types of variable
+//behaviour of variable
+//comment
+// use Strict
+//semicolon
+
+//data types
+// 1. number 
+
+// let num='20'
+// let num2=200
+// console.log(num,num2)
+
+
+// let value='9283479'
+
+// console.log(typeof value)
+
+
+// let name="bablu"
+// console.log(name)
+
+
+
+// let number ="23"
+// let number2 =25
+
+
+// // boolean//
+
+// let akashIsMale=true
+// console.log(typeof akashIsMale)
+
+// let akashIsNotEligibleToVote=false
+
+
+// redeclare reassign
+
+
+
+
+// data types
+
+// 2
+// primitive data type
+// non primitive 
+
+
+// let name ='ravikant'
+// name='shubham'
+// let number =2//#300//4 bit
+// number = 5
+// let name ='ravi'//#400//8 bit
+// name='shibham'
+
+// let array=[1,2,45,67,34,shibam]//4bit//20
+
+// let name ='shubahm'
+
+// stack //primitive data//5 mb
+
+// heap//non primitive data//
+
+
+
+
+
+// primitive data type
+
+// 1.String
+
+// let section="bia 4"
+
+// // 2. number 
+// let num=1000.45
+//-2^53+1 to 2^53-1
+
+// BigInt
+// let num2=29834725693845293487534659
+// let num2=10000002348725638470000000
+// console.log(typeof num2)
+
+//Boolean 
+
+
+// let anishIEligibleToVOte=true
+
+// console.log(typeof anishIEligibleToVOte)
+
+// let lokendraIsBillionire=false
+
+// console.log(typeof lokendraIsBillionire)
+
+
+// data type undefined
+
+
+// var name= undefined;
+// console.log(name)
+// name='ravikant'
+// console.log(name)
+
+
+// console.log(age)
+// let age;
+
+
+// null
+
+// let obj=null
+// console.log(obj)
+// obj={
+//     // sdjf;lkas
+// }
+
+
+//string ,number ,bigint ,boolean ,null, undefined,symbol
+// non primitive -array object function
+
+
+
+// alert prompt confirm 
+
+// alert('my name is shivam singh')
+
+// alert(100)
+
+// alert('I am a student of bia 4 section')
+
+
+// let name ="akash"
+// let age=16;
+
+// alert('hello my name is '+name+" and my age is "+age)
+
+// alert(`hello my name is ${name} and my age is ${age}`)//backtick
+// template literals
+
+
+
+
+let name='Etesh'
+
+let address="aliraj pur"
+
+let graduation='B.Tech'
+
+let branch='CS'
+
+console.log('My name is '+name+" My home adress is "+ address+" i have completed my ug in " + graduation + "an d my branch is "+ branch)
+
+console.log(`my name is ${name} and my home address is ${address} and i have done my UG in ${graduation} and my branch is ${branch}`)
+
+
+// Prompt
+
+// let answer=prompt('what is your name ?')
+// alert(answer)
+
+let reason =prompt('what are you doing in DDUGKY')
+
+alert(`my answer is ${reason}`)
+
+
+
---
+++ BIA $ JAVASCRIPT/variable/index.js
@@ -0,0 +1,298 @@
+// console.log(23)
+
+// //write five fruites name
+
+// take two inputs -1 1
+
+// add them
+// show result in alert
+
+// let num1=prompt('write a num')
+// let num2=prompt('write second number')
+
+// alert( Number(num1)+Number(num2))
+// alert( Number(num1)*Number(num2))
+// alert( Number(num1)-Number(num2))
+// alert( Number(num1)/Number(num2))
+
+// let firstName=30
+// let secName=50
+// console.log(firstName+secName)
+
+// take a input
+
+// power 5
+
+// let number=Number(prompt('Write a number? (please write between 1 to 1000) '))
+
+// number=Number(number)
+
+// alert("exponential "+ number**5)
+
+// let math=Number(prompt('Math marks?'))
+// let english=Number(prompt('english marks?'))
+// let hindi=Number(prompt('hindi marks?'))
+// let science=Number(prompt('science marks?'))
+// let socialScience=Number(prompt('ss marks?'))
+// let sanskrit=Number(prompt('sanskrit marks?'))
+
+// let totalMarks=math+english+hindi+science+socialScience+sanskrit
+
+// console.log(totalMarks)
+
+// let percent=(totalMarks*100)/600
+
+// console.log(percent)
+
+// alert(`student scored ${percent}% marks`)
+
+// weightin kg height in meter square
+// let name=prompt('write your name')
+// let weightInKg=Number(prompt('what is your weight in kg'))
+// let heightInCm=Number(prompt('what is your height in cm'))
+
+// let heightInMeter=heightInCm/100
+
+// let BMI=weightInKg/heightInMeter**2
+
+// alert(`My name is ${name} and my weight is ${weightInKg} and my height is ${heightInCm} and my BMI is ${BMI}`)
+
+// let userName=prompt('write your name')
+// let cityName=prompt('write your city name')
+// let tempInCel=Number(prompt('write temp of you city'))
+
+// //forehite=(tempIncel*1.8)+32
+
+// let f=(tempInCel*1.8)+32
+
+// alert(`my name is ${userName} and i am living in ${cityName} and city temperature in farenhit is ${f} and temprature in celcius is ${tempInCel}`)
+// console.log(
+//   `my name is ${userName} and i am living in ${cityName} and city temperature in farenhit is ${f} and temprature in celcius is ${tempInCel}`
+// )
+
+// first-team = CSK//230
+// second team = RCB//64
+// Third team =MI//220
+// fourth team = SRH//287
+
+// total team=4
+
+// Avg=ft+st+tt+ft/totalteam
+
+// let csk=Number(prompt('enter score of CSK'))
+// let rcb=Number(prompt('enter score of rcb'))
+// let mi=Number(prompt('enter score of mi'))
+// let srh=Number(prompt('enter score of srh'))
+// console.log(csk,rcb,mi,srh)
+
+// let totalScore=csk+rcb+mi+srh
+// console.log(totalScore)
+// let totalTeam=4;
+
+// let average=totalScore/totalTeam
+// console.log(average)
+// alert(average)
+
+// ameen reached sagar gaire
+// ameen ordered biryani
+// price of biryani
+// number of people
+// tip percneteage
+// thanku for your service
+
+// let biryaniPrice=110
+
+// let tip=15
+// alert('ameen reached sagar gaire ')
+// alert('Biryani-110 rs')
+// let orderedItem=prompt('what you want to eat')
+// let peoples=Number(prompt('number of people'))
+
+// let totalPrice=biryaniPrice*peoples
+// console.log(peoples)
+// console.log(totalPrice)
+
+// let percent=(totalPrice*tip)/100
+
+// let totalWithTip=totalPrice+percent
+
+// alert(`i ordered ${orderedItem} and I have to pay ${totalWithTip}`)
+
+// 100
+// 15
+
+// (100*15)/100
+
+// let
+
+// let itemPrice=180
+// let gst=18
+
+// let total=(itemPrice*gst)/100
+// console.log(Math.floor(total+itemPrice))
+
+// let number=16.87325//
+// let rounded=Math.floor(number)//16
+
+// +
+// -
+// *
+// /
+// **
+// %
+
+// let num=2
+
+// // num=num+1//3
+// // num=num+1//4
+// num++//num+1
+// num++//num+1
+// // num+1
+
+// console.log(num)
+
+// decrement
+
+// let num=10
+
+// // num=num-1//9
+// // num=num-1//8
+// num--//num-1//9
+// num--//num-1=8
+// num--//num-1=7
+// console.log(num)
+
+// let num=654
+// num--
+// //num653
+// console.log(--num)//52
+// num--
+// console.log(num)//51
+
+// let num=8
+
+// console.log(++num)//9
+// console.log(++num)//10
+// console.log(num--)//10//9
+// console.log(--num)//8
+// console.log(num--)//8
+// console.log(--num)//6
+
+// let num=8
+// console.log(++num)//9
+// num++
+// console.log(--num)//9
+// console.log(num++)//9
+// --num
+// console.log(--num)//8
+// console.log(num++)//8
+// console.log(num)//9
+
+// let a=93
+
+// console.log(--a)//92
+// // console.log(++b)
+// a++//93
+// --a//92
+// console.log(a--)//92
+// console.log(a)//91
+
+// >,<,==,===,<=,>=,!
+
+// let a = 10
+// let b = 20
+// console.log(a > b)
+// console.log(a < b)
+// console.log(a < 5)
+// console.log(100 < 99)
+// console.log(100 <= 101)
+// console.log(200 >= 505)
+// console.log(5 == '5')
+// console.log(5 === 5)
+// console.log('55' === 55)
+// console.log(true == 1)
+// console.log(true === 1)
+// console.log(false == 0) //true
+// console.log(false === 0) //false
+// console.log(null == 0)
+
+// console.log(false >= 0)
+
+//
+
+// let student = true
+// console.log(!student)
+// console.log(5 !== '5')
+// console.log(5>=5)
+
+// console.log(10!=='10')
+
+// console.log(10!="10")
+
+// = ,==
+
+// let a=10
+// 20==20
+// console.log(20>=50);
+
+// conditional statement
+
+// if someone is greater than 18 then he can vote
+// if(condition){
+
+// }
+
+// let age = 17
+
+// if (age >= 18) {
+//   console.log('you can give vote')
+// } else {
+//   console.log('not eligible')
+// }
+
+// let trafficLight = prompt('which light is on?(red,green,amber)')
+
+// if (trafficLight == 'red') {
+//   alert('please! stop and wait')
+// } else if (trafficLight == 'green') {
+//   alert('you can go now')
+// } else if (trafficLight == 'amber') {
+//   alert('get ready')
+// } else {
+//   alert('please give right color')
+// }
+
+// we have to take bMI value as input
+
+// if bmi is greater than 18 then normal
+// if bmi is less than 18 then underweight
+// if bmi is greater than 25 then overweight
+
+// let bmi = prompt('write you bmi')
+// console.log(bmi)
+// // 17//19//30//shovam
+// if (bmi < 1) {
+//   console.log('please enter value in postive')
+// } else if (bmi <= 18) {
+//   console.log('underweight')
+// } else if (bmi <= 25) {
+//   //19<=25
+//   console.log('normal weight')
+// } else if (bmi > 25) {
+//   console.log('overweight')
+// } else if (bmi > 50) {
+//   console.log('value should be less than 50')
+// } else {
+//   console.log('inter valid value')
+// }
+
+
+
+let myAge=12
+
+take a input of users age 
+
+if his age is greater than you then print something 
+if  his age is less than you then print something
+
+
